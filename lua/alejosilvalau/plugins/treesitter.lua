55e654fdb348924922f5e18f97ebfc2a5c1f4ae8
Alejo Silva <alejo.lautaro@hotmail.com>
2026-01-30 18:07:55 -0300

    Cleaned the treesitter config file

--- lua/alejosilvalau/plugins/treesitter.lua
+++ lua/alejosilvalau/plugins/treesitter.lua
@@ -5,11 +5,13 @@ return {
     config = function()
         local status, configs = pcall(require, "nvim-treesitter.configs")
         if not status then
+	    print("Failed to load treesitter configs")
             return
         end
 
         configs.setup({
             ensure_installed = {
+		"help",
                 "vimdoc",
                 "javascript",
                 "typescript",
@@ -28,24 +30,7 @@ return {
 
             highlight = {
                 enable = true,
-                disable = function(lang, buf)
-                    if lang == "html" then
-                        return true
-                    end
-
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        vim.notify(
-                            "File larger than 100KB: Treesitter disabled for performance",
-                            vim.log.levels.WARN,
-                            { title = "Treesitter" }
-                        )
-                        return true
-                    end
-                end,
-
-                additional_vim_regex_highlighting = { "markdown" },
+                additional_vim_regex_highlighting = false,
             },
         })
     end,
