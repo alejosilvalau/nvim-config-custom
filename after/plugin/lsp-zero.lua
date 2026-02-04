49efbcb273bcc557de5cf35347ee88b20be4c06e
Alejo Silva <alejo.lautaro@hotmail.com>
2026-02-04 16:36:18 -0300

    Solution to lsp problems and adaptation of keymaps for personal preference

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,41 +1,56 @@
 local lsp = require('lsp-zero')
 
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-end)
-
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {
-    'tsserver',
-    'eslint',
-    'lua_ls'
-  }
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
-})
+	ensure_installed = {
+		'ts_ls',
+		'eslint',
+		'lua_ls'
+	},
+		handlers = {
+		function(server_name)
+			require('lspconfig')[server_name].setup({})
+		end,
+		-- Special handler for lua_ls
+		lua_ls = function()
+			require('lspconfig').lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = {'vim'}
+						}
+					}
+				}
+			})
+		end,
+	}})
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defauls.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ['<C-Space>'] = cmp.mapping.complete(),
+
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+		['<C-y>'] = cmp.mapping.confirm({ select = true }),
+		['<C-Space>'] = cmp.mapping.complete(),
+	})
 })
 
-lsp.set_preferences({
-  sign_icons = { }
-})
+lsp.on_attach(function(client, bufnr)
+local opts = {buffer = bufnr, remap = false}
 
-
-
-
-
-
+vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+vim.keymap.set("n", "D", function() vim.lsp.buf.hover() end, opts)
+vim.keymap.set("n", "<leader>ls", function() vim.lsp.buf.workspace_symbol() end, opts)
+vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
+vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
+vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.references() end, opts)
+vim.keymap.set("n", "<leader>lR", function() vim.lsp.buf.rename() end, opts)
+vim.keymap.set("i", "<leader>lh", function() vim.lsp.buf.signature_help() end, opts)
+end)
 
 
 
