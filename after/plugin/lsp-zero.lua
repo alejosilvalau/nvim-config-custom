5038eae3e7aa64cc3536b13d8abeb62dfd273ac4
Alejo Silva <alejo.lautaro@hotmail.com>
2026-02-04 16:00:45 -0300

    Lsp-zero basic configuration done and added new keymaps to remap default window managing keymaps

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,4 +1,41 @@
-local lsp = require("lsp-zero")
+local lsp = require('lsp-zero')
 
-lsp.preset("recommended")
-lsp.setup()
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'tsserver',
+    'eslint',
+    'lua_ls'
+  }
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  },
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defauls.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<C-Space>'] = cmp.mapping.complete(),
+})
+
+lsp.set_preferences({
+  sign_icons = { }
+})
+
+
+
+
+
+
+
+
+
--- lua/alejosilvalau/config/keymaps.lua
+++ lua/alejosilvalau/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Custom shortcuts
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+-- Window navigation
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Move to down window" })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Move to up window" })
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Move to right window" })
+
+-- Window splitting
+vim.keymap.set("n", "<leader>ws", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" })
+
+-- Window closing
+vim.keymap.set("n", "<leader>wq", "<C-w>q", { desc = "Close window" })
+vim.keymap.set("n", "<leader>wo", "<C-w>o", { desc = "Close other windows" })
+
+-- Window resizing
+vim.keymap.set("n", "<leader>w=", "<C-w>=", { desc = "Equal window sizes" })
+vim.keymap.set("n", "<leader>w-", "<C-w>-", { desc = "Decrease height" })
+vim.keymap.set("n", "<leader>w+", "<C-w>+", { desc = "Increase height" })
+vim.keymap.set("n", "<leader>w<", "<C-w><", { desc = "Decrease width" })
+vim.keymap.set("n", "<leader>w>", "<C-w>>", { desc = "Increase width" })
