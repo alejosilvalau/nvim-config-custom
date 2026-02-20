18803060339fe34e4f838c708e7c70599176d7af
Alejo Silva <alejo.lautaro@hotmail.com>
2026-02-20 20:01:46 -0300

    Joined config files of lsp-zero

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,22 +1,79 @@
-return {  
+return {
   'VonHeikemen/lsp-zero.nvim',
   branch = 'v4.x',
   dependencies = {
     -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {'williamboman/mason.nvim'},           -- Optional
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    { 'neovim/nvim-lspconfig' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
 
     -- Autocompletion
-    {'hrsh7th/nvim-cmp'},         -- Required
-    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-    {'hrsh7th/cmp-buffer'},       -- Optional
-    {'hrsh7th/cmp-path'},         -- Optional
-    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'hrsh7th/cmp-nvim-lua' },
 
     -- Snippets
-    {'L3MON4D3/LuaSnip'},             -- Required
-    {'rafamadriz/friendly-snippets'}, -- Optional
-  }
+    { 'L3MON4D3/LuaSnip' },
+    { 'rafamadriz/friendly-snippets' },
+  },
+  config = function()
+    local lsp = require('lsp-zero')
+
+    require('mason').setup({})
+    require('mason-lspconfig').setup({
+      ensure_installed = {
+        'ts_ls',
+        'lua_ls',
+        'tailwindcss',
+        'html',
+        'cssls',
+        'emmet_ls'
+      },
+      handlers = {
+        function(server_name)
+          require('lspconfig')[server_name].setup({})
+        end,
+        lua_ls = function()
+          require('lspconfig').lua_ls.setup({})
+        end,
+      }
+    })
+
+    local cmp = require('cmp')
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+    cmp.setup({
+      sources = {
+        { name = 'lazydev', group_index = 0 },
+        { name = 'copilot', group_index = 2 },
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'path' },
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+      })
+    })
+
+    lsp.on_attach(function(client, bufnr)
+      local opts = { buffer = bufnr, remap = false }
+
+      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+      vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+      vim.keymap.set("n", "<leader>ls", function() vim.lsp.buf.workspace_symbol() end, opts)
+      vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
+      vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+      vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+      vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
+      vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.references() end, opts)
+      vim.keymap.set("n", "<leader>lR", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set("n", "<leader>lh", function() vim.lsp.buf.signature_help() end, opts)
+    end)
+  end
 }
