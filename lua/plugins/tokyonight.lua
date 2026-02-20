fbc8ec4bf4b3f42b870df833a042a229e6043fd2
Alejo Silva <alejo.lautaro@hotmail.com>
2026-02-20 18:49:55 -0300

    Joined config for ui on a single file

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,26 @@
+return
+{
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "night",
+    transparent = true,
+    styles = {
+      sidebars = "border_highlight",
+      floats = "border_highlight",
+    },
+
+    on_highlights = function(hl, c)
+      hl.TelescopeBorder = {
+        fg = c.border_highlight,
+      }
+      hl.FloatBorder = {
+        fg = c.border_highlight,
+      }
+    end,
+  },
+  init = function()
+    vim.cmd([[colorscheme tokyonight]])
+  end,
+}
