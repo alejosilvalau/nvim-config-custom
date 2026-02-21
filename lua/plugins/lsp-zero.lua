0971e7e71e846a998bad594f925a3f2264f25c90
Alejo Silva <alejo.lautaro@hotmail.com>
2026-02-20 21:19:24 -0300

    Solution for lsp not working properly after code changes

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -3,45 +3,66 @@ return {
   branch = 'v4.x',
   dependencies = {
     -- LSP Support
-    {'neovim/nvim-lspconfig'},             
-    {'williamboman/mason.nvim'},           
-    {'williamboman/mason-lspconfig.nvim'}, 
+    { 'neovim/nvim-lspconfig' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
 
     -- Autocompletion
-    {'hrsh7th/nvim-cmp'},         
-    {'hrsh7th/cmp-nvim-lsp'},     
-    {'hrsh7th/cmp-buffer'},       
-    {'hrsh7th/cmp-path'},         
-    {'saadparwaiz1/cmp_luasnip'}, 
-    {'hrsh7th/cmp-nvim-lua'},     
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'hrsh7th/cmp-nvim-lua' },
 
     -- Snippets
-    {'L3MON4D3/LuaSnip'},             
-    {'rafamadriz/friendly-snippets'}, 
+    { 'L3MON4D3/LuaSnip' },
+    { 'rafamadriz/friendly-snippets' },
   },
   config = function()
     local lsp = require('lsp-zero')
 
     require('mason').setup({})
     require('mason-lspconfig').setup({
-      ensure_installed = { 'ts_ls', 'lua_ls', 'tailwindcss' },
+      ensure_installed = {
+        'ts_ls',
+        'lua_ls',
+        'tailwindcss',
+        'html',
+        'cssls',
+        'emmet_ls' },
       handlers = {
         function(server_name)
           require('lspconfig')[server_name].setup({})
+        end,
+        lua_ls = function()
+          require('lspconfig').lua_ls.setup({
+          })
         end,
       }
     })
 
     local cmp = require('cmp')
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
     cmp.setup({
-      sources = { { name = 'nvim_lsp' }, { name = 'buffer' } },
+      sources = {
+        { name = 'lazydev', group_index = 0 },
+        { name = 'copilot', group_index = 2 },
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'path' },
+      },
       mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
       })
     })
   end,
   keys = {
-        { "gd",         function() vim.lsp.buf.definition() end,       desc = "LSP Definition" },
+    { "gd",         function() vim.lsp.buf.definition() end,       desc = "LSP Definition" },
     { "K",          function() vim.lsp.buf.hover() end,            desc = "LSP Hover" },
     { "<leader>ls", function() vim.lsp.buf.workspace_symbol() end, desc = "LSP Workspace Symbol" },
     { "<leader>ld", function() vim.diagnostic.open_float() end,    desc = "LSP Diagnostic Float" },
