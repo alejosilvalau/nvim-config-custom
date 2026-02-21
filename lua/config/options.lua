4838ee9e5f4055ededa602640c3aad43c2285729
Alejo Silva <alejo.lautaro@hotmail.com>
2026-02-20 21:44:49 -0300

    Moved undotree config to be only on the undotree.lua file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,11 +9,6 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv('HOME') .. "/.vim/undodir"
-vim.opt.undofile = true
-
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,6 +1,12 @@
 return {
-    "mbbill/undotree",
-    keys = {
-        {"<leader>u", "<cmd>UndotreeToggle<cr>", mode = "n", desc = "Undotree Toggle"},
-    },
+  "mbbill/undotree",
+  keys = {
+    { "<leader>u", "<cmd>UndotreeToggle<cr>", mode = "n", desc = "Undotree Toggle" },
+  },
+  config = function()
+    vim.opt.swapfile = false
+    vim.opt.backup = false
+    vim.opt.undodir = os.getenv('HOME') .. "/.vim/undodir"
+    vim.opt.undofile = true
+  end,
 }
