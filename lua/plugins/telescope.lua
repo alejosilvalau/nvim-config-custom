194c2a58e0542f0bd627864a2e382ddea460a624
Alejo Silva <alejo.lautaro@hotmail.com>
2026-03-07 18:10:13 -0300

    Added new keymap for searching strings within a buffer with telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,15 +7,14 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   keys = {
-    { "<leader>ff", "<cmd>Telescope find_files<cr>",                                                            desc = "Find Files" },
-    { "<leader>fG", "<cmd>Telescope git_files<cr>",                                                             desc = "Find Git Files" },
-    { "<leader>fg", "<cmd>Telescope live_grep<cr>",                                                             desc = "Live Grep" },
-    { "<leader>fh", "<cmd>Telescope help_tags<cr>",                                                             desc = "Search Help" },
-    { "<leader>fk", "<cmd>Telescope keymaps<cr>",                                                               desc = "Search Keymaps" },
-    { "<leader>f*", "<cmd>Telescope grep_string<cr>",                                                           desc = "Search Current Word" },
-    { "<leader>fd", "<cmd>Telescope diagnostics<cr>",                                                           desc = "Search Diagnostics" },
-    { "<leader>fo", "<cmd>Telescope oldfiles<cr>",                                                              desc = "Search Old Files" },
-    { "<leader>fn", "<cmd>lua require('telescope.builtin').find_files({ cwd = vim.fn.stdpath('config') })<cr>", desc = "Search Neovim files" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>",  desc = "Find Files" },
+    { "<leader>fG", "<cmd>Telescope git_files<cr>",   desc = "Find Git Files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>",   desc = "Live Grep" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>",   desc = "Search Help" },
+    { "<leader>fk", "<cmd>Telescope keymaps<cr>",     desc = "Search Keymaps" },
+    { "<leader>f*", "<cmd>Telescope grep_string<cr>", desc = "Search Current Word" },
+    { "<leader>fd", "<cmd>Telescope diagnostics<cr>", desc = "Search Diagnostics" },
+    { "<leader>fo", "<cmd>Telescope oldfiles<cr>",    desc = "Search Old Files" },
   },
   opts = {
     defaults = {
@@ -41,5 +40,18 @@ return {
         no_ignore = true,
       },
     },
-  }
+  },
+  config = function(_, opts)
+    require("telescope").setup(opts)
+    vim.keymap.set("n", "<leader>fn", function()
+      require("telescope.builtin").find_files { cwd = vim.fn.stdpath "config" }
+    end, { desc = "Search Neovim files" })
+
+    vim.keymap.set("n", "<leader>/", function()
+      require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+        winblend = 10,
+        previewer = false,
+      })
+    end, { desc = "Fuzzily search in current buffer" })
+  end,
 }
