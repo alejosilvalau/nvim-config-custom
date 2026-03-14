604a79d772d7fb537b2d1ec0b153151a61beb268
Alejo Silva <alejo.lautaro@hotmail.com>
2026-03-14 17:46:52 -0300

    Added template note header for new notes

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -34,6 +34,13 @@ return {
     ui = {
       enable = false
     },
+    note_frontmatter_func = function(note)
+      return {
+        date    = os.date("%Y-%m-%d"),
+        aliases = note.aliases or {},
+        tags    = note.tags or {},
+      }
+    end,
   },
   -- Checkbox toggle
   config = function(_, opts)
@@ -44,6 +51,8 @@ return {
         local ok, obsidian = pcall(require, "obsidian")
         if not ok then return end
         if obsidian.get_client() == nil then return end
+
+        vim.keymap.set("n", "<leader>no", ":ObsidianTemplate note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>")
 
         vim.keymap.set("n", "<leader>nt", "<cmd>ObsidianTags<cr>", { desc = "Obsidian Tags" })
 
