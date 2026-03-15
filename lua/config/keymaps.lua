2a4d4c0fa3ab0d227fdb0f25b3f7d2a8af929d5c
Alejo Silva <alejo.lautaro@hotmail.com>
2026-03-15 09:50:42 -0300

    Improved coherence on keymaps file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -45,7 +45,6 @@ end
 
 vim.keymap.set("n", "<leader>wr", resize_mode, { desc = "Enter resize mode" })
 
-
 -- Move selected code
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
