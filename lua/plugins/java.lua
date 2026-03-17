254d4950a36eb9e813c91a589b0477275c3b6aa6
Alejo Silva <alejo.lautaro@hotmail.com>
2026-03-17 09:15:12 -0300

    Added more keymaps to the java plugin for better integration with the dap-ui

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -9,17 +9,25 @@ return {
     { '<leader>jr',  function() require('java').runner.built_in.run_app() end,     ft = 'java', desc = 'Run app' },
     { '<leader>js',  function() require('java').runner.built_in.stop_app() end,    ft = 'java', desc = 'Stop app' },
     { '<leader>jl',  function() require('java').runner.built_in.toggle_logs() end, ft = 'java', desc = 'Toggle logs' },
+
     -- Tests
     { '<leader>jtc', function() require('java').test.run_current_class() end,      ft = 'java', desc = 'Test class' },
     { '<leader>jtm', function() require('java').test.run_current_method() end,     ft = 'java', desc = 'Test method' },
     { '<leader>jta', function() require('java').test.run_all_tests() end,          ft = 'java', desc = 'Test all' },
     { '<leader>jtr', function() require('java').test.view_last_report() end,       ft = 'java', desc = 'Test report' },
+
+    -- Debug via DAP-UI
+    { '<leader>jtD', function() require('java').test.debug_current_class() end,    ft = 'java', desc = 'Debug class' },
+    { '<leader>jtd', function() require('java').test.debug_current_method() end,   ft = 'java', desc = 'Debug method' },
+
     -- Refactor
     { '<leader>jxv', function() require('java').refactor.extract_variable() end,   ft = 'java', desc = 'Extract variable' },
     { '<leader>jxm', function() require('java').refactor.extract_method() end,     ft = 'java', desc = 'Extract method' },
     { '<leader>jxc', function() require('java').refactor.extract_constant() end,   ft = 'java', desc = 'Extract constant' },
+
     -- Misc
     { '<leader>jp',  function() require('java').profile.ui() end,                  ft = 'java', desc = 'Profiles' },
+    { '<leader>ji',  function() require('java').project.import_settings() end,     ft = 'java', desc = 'Import Settings' },
     { '<leader>jR',  function() require('java').settings.change_runtime() end,     ft = 'java', desc = 'Change JDK' },
   },
 }
