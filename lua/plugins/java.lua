3dab1a281b91339de0c512763105451ac458e2a2
Alejo Silva <alejo.lautaro@hotmail.com>
2026-03-18 12:59:28 -0300

    Upgraded jdtls ready message

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -62,7 +62,7 @@ return {
       callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
         if client and client.name == 'jdtls' then
-          vim.notify('JDTLS ready', vim.log.levels.INFO)
+          vim.notify('[JDTLS ready]', vim.log.levels.INFO)
         end
       end,
     })
