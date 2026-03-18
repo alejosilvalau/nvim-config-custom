0fd2679a18fc729c6f2e2772e3fa094ebd3a88ea
Alejo Silva <alejo.lautaro@hotmail.com>
2026-03-17 21:08:49 -0300

    Almost got java working with neovim

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,12 +1,25 @@
 return {
   'nvim-java/nvim-java',
+  ft = 'java',
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'JavaHello/spring-boot.nvim',
+  },
   config = function()
-    require('java').setup()
+    require('java').setup({
+      spring_boot_tools = {
+        enable = true,
+      },
+    })
+    vim.lsp.config('jdtls', {
+      root_dir = vim.fs.root(0, { ".project", "pom.xml", "gradle.build", ".git" }),
+    })
     vim.lsp.enable('jdtls')
   end,
   keys = {
     -- Runner
-    { '<leader>jr',  function() require('java').runner.built_in.run_app() end,     ft = 'java', desc = 'Run app' },
+    { '<leader>jb',  function() require('java').build.build_workspace() end,       ft = 'java', desc = 'Build workspace' },
+    { '<leader>jr',  function() require('java').runner.built_in.run_app({}) end,   ft = 'java', desc = 'Run app' },
     { '<leader>js',  function() require('java').runner.built_in.stop_app() end,    ft = 'java', desc = 'Stop app' },
     { '<leader>jl',  function() require('java').runner.built_in.toggle_logs() end, ft = 'java', desc = 'Toggle logs' },
 
