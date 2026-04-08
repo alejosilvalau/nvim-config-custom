325d893af6726a542bc5a48dc7b9d7e3f7e59d98
Alejo Silva <alejo.lautaro@hotmail.com>
2026-04-07 23:03:36 -0300

    Added options to indent breakpoints on text

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,6 +8,9 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.breakindent = true
+vim.opt.showbreak = "↳ "
+vim.opt.wrap = true
 
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
